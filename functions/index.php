b735aaaf8bdb11a4ae23cb107d36bc0cd4d7c4b5
Psdannb <psdannb@gmail.com>
2026-06-22 11:41:26 +0545

    end of chapter 1

--- functions/index.php
+++ functions/index.php
@@ -1,15 +1,17 @@
 <?php
-include"../tasks/index.php";
+include "../tasks/index.php";
 // echo "I am running from the functions /index.php";
 
-function greet(){
-   echo "I am running from the greet function"; 
+function greet()
+{
+    echo "I am running from the greet function";
 }
 // greet();
-function sum($a,$b){
+function sum($a, $b)
+{
     // $a=6;
     // $b=5;
-    $c=$a+$b;
+    $c = $a + $b;
     return $c;
     // echo $c;
     // echo "<br>";
@@ -22,11 +24,45 @@ function sum($a,$b){
 // echo $c;
 
 
-function multiply($a,$b){
-    $c=$a*$b;
+function multiply($a, $b)
+{
+    $c = $a * $b;
     return $c;
 }
 
 
-welcome("Nawaraj");
-?>
+// welcome("Nawaraj");
+
+
+function addition(...$numbers)
+{
+    $sum = 0;
+    // var_dump($numbers);
+    foreach ($numbers as $number) {
+        $sum = $sum + $number;
+        echo $sum;
+        echo "<br>";
+    }
+    // return $sum;
+}
+// addition(2, 40, 50, 50, 3, 45);
+// $output = addition(2, 40, 50, 50, 3, 45);
+// echo $output;
+
+
+function callByValue($a)
+{
+    $a++;
+    echo "I am inside the function and the value of variable a inside the function is  " . $a;
+}
+function callByReference(&$a)
+{
+    $a++;
+    echo "I am inside the function and the value of variable a inside the function is  " . $a;
+}
+
+$a = 5;
+// callByValue($a);
+callByReference($a);
+echo "<br>";
+echo "I am outside the function and the value of a is  " . $a;
--- tasks/index.php
+++ tasks/index.php
@@ -1,30 +1,32 @@
 <?php
-function welcome($name){
-    echo "Welcome ".$name;
+function welcome($name)
+{
+    echo "Welcome " . $name;
 }
 // welcome("Rohit");
 
-function calculation($operation,$operand1,$operand2){
-    switch($operation){
-        case "+":{
-            return $operand1+$operand2;
-            break;
-        }
-        case "-":{
-        return $operand1-$operand2;
-        break;
-        }
-        case "*":{
-            return $operand1*$operand2;
-            break;
-        }
-        case "/":{
-            return $operand1/$operand2;
-            break;
-        }
-        default:{
-            return "Invalid operation";
-        }
+function calculation($operation, $operand1, $operand2)
+{
+    switch ($operation) {
+        case "+": {
+                return $operand1 + $operand2;
+                break;
+            }
+        case "-": {
+                return $operand1 - $operand2;
+                break;
+            }
+        case "*": {
+                return $operand1 * $operand2;
+                break;
+            }
+        case "/": {
+                return $operand1 / $operand2;
+                break;
+            }
+        default: {
+                return "Invalid operation";
+            }
     }
 }
 // $result=calculation("+",5,7);
@@ -37,4 +39,3 @@ function calculation($operation,$operand1,$operand2){
 
 // Todo 
 // how to create a function which takes arbitary number of parameters 
-?>
