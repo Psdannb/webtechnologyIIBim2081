a501d88164c4e7864832d603d32f8a3db52f73d9
Psdannb <psdannb@gmail.com>
2026-07-08 11:45:27 +0545

    lab 1 updated

--- lab/lab1.php
+++ lab/lab1.php
@@ -1,20 +1,24 @@
 <?php
-$raw_products = ["    t-shirt:25.99:5 ", "JEANS:49.95:0", " jacket : 89.00 : 2 "];
+$raw_products = ["    t-shirt:25.99:5    ", "JEANS:49.95:0", "    jacket : 89.00 : 2 "];
 // print_r($raw_products);
 $newproductsAssoc=[];
 foreach($raw_products as $product) {
-//   print_r($product);
-//   echo "<br>";
+  // var_dump($product);
+  // echo "<br>";
 $whiteSpaceRemovedProduct = trim($product);
+  // var_dump($whiteSpaceRemovedProduct);
+  // echo "<br>";
 $lowercase=strtolower($whiteSpaceRemovedProduct);
+// print_r($lowercase);
+// echo "<br>";
 $newProductArray= explode(":", $lowercase);
+// print_r($newProductArray);
+// echo "<br>";
 $newproductsAssoc[] = [
     "name" => $newProductArray[0],
     "price" => $newProductArray[1],
     "quantity" => $newProductArray[2]
   ];
-
-
 }
   print_r($newproductsAssoc);
 
