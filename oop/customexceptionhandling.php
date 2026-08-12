08645f82c1fec24dea753e86b262437b38d6d950
Psdannb <psdannb@gmail.com>
2026-08-12 08:42:32 +0545

    exception handling more example

---
+++ oop/customexceptionhandling.php
@@ -0,0 +1,43 @@
+<?php
+/*
+#*** Custom Exception Handling **#
+A custom exception is a user-defined exception class created by the programmer for handling specific types of errors or exceptional situations in an application.
+
+
+syntax: 
+class MyException extends Exception
+{
+    // Custom exception code
+}
+
+Example: Suppose we want to check whether a student is eligible for admission.
+
+
+class AgeException extends Exception
+{
+    public function showMessage()
+    {
+        return "Invalid age entered.";
+    }
+}
+
+$age = 15;
+
+try {
+
+    if ($age < 18) {
+        throw new AgeException("Age must be 18 or above.");
+    }
+
+}
+catch (AgeException $e) {
+
+    echo $e->showMessage() . "<br>";
+    echo $e->getMessage();
+
+}
+
+
+*/
+
+?>
--- oop/exceptionhandling.php
+++ oop/exceptionhandling.php
@@ -61,24 +61,38 @@ It is commonly used for cleanup operations such as:
 
 
  Example 1:
+
 try {
 
-    throw new Exception("Something went wrong.");
+    throw new Exception("Something went wrong.","101");
 
 }
 catch (Exception $e) {
 
     echo "Error: " . $e->getMessage();
-
+    echo "<br>";
+    echo "Error code: ".$e->getCode();
+    echo "<br>";
+    echo "Error occured in file :". $e->getFile();
+    echo "<br>";
+    echo "Error occured in line number :". $e->getLine();
+     echo "<br>";
 }
+finally{
+    echo "I am clean up code";
+}
+     */
+
+/*
 Example -2
+
 try {
 
     echo "Program started.<br>";
 
     throw new Exception("An error occurred.");
 
-    echo "This line will not execute.";
+    echo "This line will not execute.<br>";
 
 }
 catch (Exception $e) {
@@ -91,10 +105,12 @@ finally {
     echo "Finally block executed.";
 
 }
-
+    */
+/*
 Example-3
 
-$age = 15;
+
+$age = 19;
 
 try {
 
@@ -110,7 +126,38 @@ catch (Exception $e) {
     echo "Error: " . $e->getMessage();
 
 }
-
 */
+/*
+example-4
+$a=5;
+$b=1;
+try{
+if($b==0){
+    throw new Exception ("Divide by zero is not possible");
+}
+$c=$a/$b;
+echo "The result is :".$c;
+}
+catch(Exception $excp){
+    echo "Error : ". $excp->getMessage();
+}
+/*
+
+/* exception handling using function 
+*/
+function sum($a,$b){
+if($b==0){
+    throw new Exception ("Divide by zero is not possible");
+}
+$c=$a/$b;
+echo "The result is :".$c;
+
+}
+try{
+    sum(2,5);
+}
+catch(Exception $xyz){
+    echo "Error: ".$xyz->getMessage();
+}
 
 ?>
